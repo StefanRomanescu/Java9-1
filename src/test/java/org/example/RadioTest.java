19d36a5ac713f95cad774ec3289cf200b54542c2
netotest <stepanmokan@ya.ru>
2024-03-02 20:16:17 +0300

    Coverage100

--- src/test/java/org/example/RadioTest.java
+++ src/test/java/org/example/RadioTest.java
@@ -172,4 +172,23 @@ public class RadioTest {
         int actual = radio.getCurrentVolume();
         Assertions.assertEquals(expected, actual);
     }
+
+    @Test
+    public void shouldSetVolumeOverMax() {
+
+        radio.setCurrentVolume(101);
+
+        int expected = 0;
+        int actual = radio.getCurrentVolume();
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    public void shouldSetVolumeOverMin() {
+
+        radio.setCurrentVolume(-1);
+
+        int expected = 0;
+        int actual = radio.getCurrentVolume();
+        Assertions.assertEquals(expected, actual);
+    }
 }
